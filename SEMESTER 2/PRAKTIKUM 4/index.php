11082da456611acba04577ba65ec88cf0fc4f2fc
satriatf <satriatf11@gmail.com>
2024-03-17 23:07:01 +0700

    PRAKTIKUM DAN TUGAS

---
+++ SEMESTER 2/PRAKTIKUM 4/index.php
@@ -0,0 +1,20 @@
+<?php 
+$username = $_POST['username'];
+$password = $_POST['password'];
+$submit = $_POST['submit'];
+
+if (!isset($submit)) {
+    header("Location: login.php");
+};
+
+if ($username === 'satria' &&  $password === "123" ) {
+    echo "<script>alert('Selamat Datang!')</script>";
+}else{
+    header("Location: login.php");
+};
+
+include_once 'layouts/top.php';
+include_once 'layouts/menu.php';
+include_once 'layouts/bottom.php';
+
+?>  
---
+++ SEMESTER 2/PRAKTIKUM 4/layouts/bottom.php
@@ -0,0 +1,20 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+	<!-- REQUIRED SCRIPTS -->
+	<!-- jQuery -->
+	<script src="assets/plugin/jquery/jquery.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="assets/plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- AdminLTE -->
+	<script src="assets/dist/js/adminlte.js"></script>
+	<!-- OPTIONAL SCRIPTS -->
+	<script src="assets/plugin/chart.js/Chart.min.css"></script>
+</body>
+</html>
